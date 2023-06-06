750f7efa236577de4ddb8756afe899e679b696bb
ZSabakh <zsabakhtarishvili@atu.edu>
2023-06-06 15:03:16 -0500

    Added test

---
+++ node/test/test.js
@@ -0,0 +1,11 @@
+const { expect } = require("chai");
+const app = require("../app");
+const request = require("supertest")(app);
+
+describe("GET /", () => {
+  it("should return status 200 and json object with title property", async () => {
+    const res = await request.get("/");
+    expect(res.status).to.equal(200);
+    expect(res.body).to.have.property("title");
+  });
+});
