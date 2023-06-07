8a723b6df9f224266f1edacd2201efdd4ca69bd9
ZSabakh <zsabakhtarishvili@atu.edu>
2023-06-07 12:42:08 -0500

    fixed test

--- node/test/test.js
+++ node/test/test.js
@@ -6,6 +6,6 @@ describe("GET /", () => {
   it("should return status 200 and json object with title property", async () => {
     const res = await request.get("/");
     expect(res.status).to.equal(200);
-    expect(res.body).to.have.property("title");
+    expect(res.body).to.have.property("text");
   });
 });
